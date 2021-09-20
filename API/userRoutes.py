eaa6e1ce6224b42808e23d9299f786f3f044608d
StubbornSparkle <lydiay711@gmail.com>
2021-09-20 11:40:21 +0200

    mail stuff

--- API/userRoutes.py
+++ API/userRoutes.py
@@ -187,19 +187,19 @@ def createUser():
 
             db.araf(info)
 
-            # app.config['MAIL_SERVER']='smtp.gmail.com'
-            # app.config['MAIL_PORT'] = 465
-            # app.config['MAIL_USERNAME'] = 'bikeroonsApp@gmail.com'
-            # app.config['MAIL_PASSWORD'] = "Ll1234567"
-            # app.config['MAIL_USE_TLS'] = False
-            # app.config['MAIL_USE_SSL'] = True
+            app.config['MAIL_SERVER']='smtp.gmail.com'
+            app.config['MAIL_PORT'] = 465
+            app.config['MAIL_USERNAME'] = 'bikeroonsApp@gmail.com'
+            app.config['MAIL_PASSWORD'] = "Ll1234567"
+            app.config['MAIL_USE_TLS'] = False
+            app.config['MAIL_USE_SSL'] = True
 
-            # mail = Mail(app)
+            mail = Mail(app)
 
-            # msg = Message('Welcome to Bikeroons!', sender = 'BikeroonsApp@gmail.com', recipients = [request.form['email']])
-            # msg.body = f"We are thrilled to have you become a member of the Bikeroons community\nYour Login information is:\n\nE-mail: {request.form['email']}\nPassword: {request.form['password']} "
-            # mail.send(msg)
-            # return "Sent"
+            msg = Message('Welcome to Bikeroons!', sender = 'BikeroonsApp@gmail.com', recipients = [request.form['email']])
+            msg.body = f"We are thrilled to have you become a member of the Bikeroons community\nYour Login information is:\n\nE-mail: {request.form['email']}\nPassword: {request.form['password']} "
+            mail.send(msg)
+            return "Sent"
 
             out = json.dumps(message, default=str)
             resp = flask.make_response(out)
